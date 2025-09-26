8ee1d569fbf97a64c56b8881c49b2fc32c1e1cd1
Deckard Gerritsen <deckardger@gmail.com>
2025-09-26 18:31:49 +0800

    Added proper mobile breadcrumbs support

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -7,40 +7,69 @@ import {
   BreadcrumbLink,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { HoverPrefetchLink } from '@/components/ui/hover-prefetch-link';
 import { ThemeToggle } from '@/components/dashboard/ThemeToggle';
 import { Separator } from '@/components/ui/separator';
+import { SidebarTrigger } from '@/components/ui/sidebar';
 import { type LucideIcon, Bell } from 'lucide-react';
 
 type HeaderProps = {
   breadcrumbs: { href: string; label: string; icon?: LucideIcon }[];
 };
 
+// TODO: If converted to a client component in the future, add SidebarTrigger state shown when sidebar is collapsed
 export function Header({ breadcrumbs }: HeaderProps) {
   return (
     <header className="flex items-center justify-between">
-      <Breadcrumb>
-        <BreadcrumbList>
-          {breadcrumbs.flatMap((breadcrumb, index) => {
-            const items = [
-              <BreadcrumbItem key={`${breadcrumb.href}-item`}>
-                <BreadcrumbLink href={breadcrumb.href} asChild>
-                  <HoverPrefetchLink href={breadcrumb.href}>
-                    {breadcrumb.icon && <breadcrumb.icon className="size-4" />}
-                    {breadcrumb.label}
-                  </HoverPrefetchLink>
-                </BreadcrumbLink>
-              </BreadcrumbItem>,
-            ];
+      <div className="flex items-center gap-2">
+        <SidebarTrigger className="flex md:hidden" />
+        <Breadcrumb>
+          <BreadcrumbList className="hidden min-[512px]:flex">
+            {breadcrumbs.flatMap((breadcrumb, index) => {
+              const items = [
+                <BreadcrumbItem key={`${breadcrumb.href}-item`}>
+                  <BreadcrumbLink asChild>
+                    <HoverPrefetchLink href={breadcrumb.href}>
+                      {breadcrumb.icon && <breadcrumb.icon className="size-4" />}
+                      {breadcrumb.label}
+                    </HoverPrefetchLink>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>,
+              ];
 
-            if (index < breadcrumbs.length - 1) {
-              items.push(<BreadcrumbSeparator key={`${breadcrumb.href}-separator`} />);
-            }
+              if (index < breadcrumbs.length - 1) {
+                items.push(<BreadcrumbSeparator key={`${breadcrumb.href}-separator`} />);
+              }
 
-            return items;
-          })}
-        </BreadcrumbList>
-      </Breadcrumb>
+              return items;
+            })}
+          </BreadcrumbList>
+          <BreadcrumbList className="flex min-[512px]:hidden">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="flex items-center gap-1">
+                <BreadcrumbItem>
+                  <BreadcrumbLink>
+                    <span>{breadcrumbs[breadcrumbs.length - 1].label}</span>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="start">
+                {breadcrumbs.map((breadcrumb) => (
+                  <DropdownMenuItem key={`${breadcrumb.href}-item`} asChild>
+                    <HoverPrefetchLink href={breadcrumb.href}>{breadcrumb.label}</HoverPrefetchLink>
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
       <div className="flex items-center gap-2">
         <Button variant="ghost" className="relative">
           <svg width="800px" height="800px" viewBox="0 0 56 56">
